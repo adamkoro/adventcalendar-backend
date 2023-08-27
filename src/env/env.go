196541b6a4c9c462fba2099a396bcff35e936cef
adamkoro <adam.koronics@gmail.com>
2023-08-27 15:57:37 +0200

    feat: db envs

--- src/env/env.go
+++ src/env/env.go
@@ -12,3 +12,67 @@ func GetHttpPort() int {
 	}
 	return port
 }
+
+func GetDbHost() string {
+	host := "localhost"
+	if os.Getenv("DB_HOST") != "" {
+		host = os.Getenv("DB_HOST")
+	}
+	return host
+}
+
+func GetDbPort() int {
+	port := 5432
+	if os.Getenv("DB_PORT") != "" {
+		port, _ = strconv.Atoi(os.Getenv("DB_PORT"))
+	}
+	return port
+}
+
+func GetDbUser() string {
+	user := "postgres"
+	if os.Getenv("DB_USER") != "" {
+		user = os.Getenv("DB_USER")
+	}
+	return user
+}
+
+func GetDbPassword() string {
+	password := "postgres"
+	if os.Getenv("DB_PASSWORD") != "" {
+		password = os.Getenv("DB_PASSWORD")
+	}
+	return password
+}
+
+func GetDbName() string {
+	name := "postgres"
+	if os.Getenv("DB_NAME") != "" {
+		name = os.Getenv("DB_NAME")
+	}
+	return name
+}
+
+func GetDbSslMode() string {
+	sslMode := "disable"
+	if os.Getenv("DB_SSL_MODE") != "" {
+		sslMode = os.Getenv("DB_SSL_MODE")
+	}
+	return sslMode
+}
+
+func GetDbMaxIdleConns() int {
+	maxIdleConns := 0
+	if os.Getenv("DB_MAX_IDLE_CONNS") != "" {
+		maxIdleConns, _ = strconv.Atoi(os.Getenv("DB_MAX_IDLE_CONNS"))
+	}
+	return maxIdleConns
+}
+
+func GetDbMaxOpenConns() int {
+	maxOpenConns := 0
+	if os.Getenv("DB_MAX_OPEN_CONNS") != "" {
+		maxOpenConns, _ = strconv.Atoi(os.Getenv("DB_MAX_OPEN_CONNS"))
+	}
+	return maxOpenConns
+}
--- src/env/env_test.go
+++ src/env/env_test.go
@@ -18,3 +18,99 @@ func TestGetHttpPort(t *testing.T) {
 		t.Errorf("expected %d, got %d", expected, actual)
 	}
 }
+
+func TestGetDbHost(t *testing.T) {
+	os.Setenv("DB_HOST", "localhost")
+	defer os.Unsetenv("DB_HOST")
+
+	expected := "localhost"
+	actual := env.GetDbHost()
+
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestGetDbPort(t *testing.T) {
+	os.Setenv("DB_PORT", "5432")
+	defer os.Unsetenv("DB_PORT")
+
+	expected := 5432
+	actual := env.GetDbPort()
+
+	if actual != expected {
+		t.Errorf("expected %d, got %d", expected, actual)
+	}
+}
+
+func TestGetDbUser(t *testing.T) {
+	os.Setenv("DB_USER", "postgres")
+	defer os.Unsetenv("DB_USER")
+
+	expected := "postgres"
+	actual := env.GetDbUser()
+
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestGetDbPassword(t *testing.T) {
+	os.Setenv("DB_PASSWORD", "postgres")
+	defer os.Unsetenv("DB_PASSWORD")
+
+	expected := "postgres"
+	actual := env.GetDbPassword()
+
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestGetDbName(t *testing.T) {
+	os.Setenv("DB_NAME", "postgres")
+	defer os.Unsetenv("DB_NAME")
+
+	expected := "postgres"
+	actual := env.GetDbName()
+
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestGetDbSslMode(t *testing.T) {
+	os.Setenv("DB_SSL_MODE", "disable")
+	defer os.Unsetenv("DB_SSL_MODE")
+
+	expected := "disable"
+	actual := env.GetDbSslMode()
+
+	if actual != expected {
+		t.Errorf("expected %s, got %s", expected, actual)
+	}
+}
+
+func TestGetDbMaxIdleConns(t *testing.T) {
+	os.Setenv("DB_MAX_IDLE_CONNS", "10")
+	defer os.Unsetenv("DB_MAX_IDLE_CONNS")
+
+	expected := 10
+	actual := env.GetDbMaxIdleConns()
+
+	if actual != expected {
+		t.Errorf("expected %d, got %d", expected, actual)
+	}
+}
+
+func TestGetDbMaxOpenConns(t *testing.T) {
+	os.Setenv("DB_MAX_OPEN_CONNS", "10")
+	defer os.Unsetenv("DB_MAX_OPEN_CONNS")
+
+	expected := 10
+	actual := env.GetDbMaxOpenConns()
+
+	if actual != expected {
+		t.Errorf("expected %d, got %d", expected, actual)
+	}
+}
