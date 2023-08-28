6811f470411a28ac14ec4b9625b93b42192a2bb9
adamkoro <adam.koronics@gmail.com>
2023-08-28 13:44:19 +0200

    feat: "not null" checks

--- src/postgres/structs.go
+++ src/postgres/structs.go
@@ -3,10 +3,10 @@ package db
 import "time"
 
 type User struct {
-	Key        uint   `gorm:"primaryKey"`
-	Username   string `gorm:"unique"`
-	Email      string `gorm:"unique"`
-	Password   string
+	Key        uint      `gorm:"primaryKey"`
+	Username   string    `gorm:"unique, not null"`
+	Email      string    `gorm:"unique, not null"`
+	Password   string    `gorm:"not null"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"`
 	ModifiedAt time.Time `gorm:"autoUpdateTime"`
 }
