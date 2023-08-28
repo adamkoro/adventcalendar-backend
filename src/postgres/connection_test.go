51f928e4a603f5f157abe2fa7c792551fadb3f26
adamkoro <adam.koronics@gmail.com>
2023-08-28 17:31:57 +0200

    test: db test

---
+++ src/postgres/connection_test.go
@@ -0,0 +1,36 @@
+package postgres_test
+
+import (
+	"testing"
+
+	"github.com/adamkoro/adventcalendar-backend/postgres"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConnect(t *testing.T) {
+	db, err := postgres.Connect()
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+}
+
+func TestMigrate(t *testing.T) {
+	db, _ := postgres.Connect()
+	err := postgres.Migrate(db)
+	assert.Nil(t, err)
+}
+
+func TestClose(t *testing.T) {
+	db, _ := postgres.Connect()
+	err := postgres.Close(db)
+	assert.Nil(t, err)
+}
+
+func TestCreateUser(t *testing.T) {
+
+	db, _ := postgres.Connect()
+	postgres.Migrate(db)
+	defer postgres.Close(db)
+
+	err := postgres.CreateUser(db, "testuser", "test@domain.test", "testpassword")
+	assert.Nil(t, err)
+}
