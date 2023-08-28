16b7b365fd5e5b38ad9c52741cd2a2577af3abe6
adamkoro <adam.koronics@gmail.com>
2023-08-28 20:26:35 +0200

    chore: refactor

--- src/main.go
+++ src/main.go
@@ -12,7 +12,7 @@ import (
 
 	endpoints "github.com/adamkoro/adventcalendar-backend/api"
 	"github.com/adamkoro/adventcalendar-backend/env"
-	postgres "github.com/adamkoro/adventcalendar-backend/postgres"
+	"github.com/adamkoro/adventcalendar-backend/postgres"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -72,22 +72,15 @@ func main() {
 		}
 	}()
 
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
 	quit := make(chan os.Signal)
-	// kill (no param) default send syscanll.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
 	log.Println("Shutdown Server ...")
-
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := server.Shutdown(ctx); err != nil {
 		log.Fatal("Server Shutdown:", err)
 	}
-	// catching ctx.Done(). timeout of 5 seconds.
 	select {
 	case <-ctx.Done():
 		log.Println("timeout of 5 seconds.")
