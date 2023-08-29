cc478529e0bc9c767e5a8642d30b279f879ba172
adamkoro <adam.koronics@gmail.com>
2023-08-29 16:00:01 +0200

    feat(cache): redis

---
+++ src/redis/connection.go
@@ -0,0 +1,38 @@
+package redis
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/adamkoro/adventcalendar-backend/env"
+	"github.com/redis/go-redis/v9"
+)
+
+func Connect() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     env.GetRedisHost() + ":" + strconv.Itoa(env.GetRedisPort()),
+		Password: env.GetRedisPassword(),
+		DB:       env.GetRedisDb(),
+	})
+}
+
+func Close(client *redis.Client) {
+	client.Close()
+}
+
+func Ping(client *redis.Client) error {
+	_, err := client.Ping(context.Background()).Result()
+	return err
+}
+
+func Set(client *redis.Client, key string, value interface{}) error {
+	return client.Set(context.Background(), key, value, 0).Err()
+}
+
+func Get(client *redis.Client, key string) (string, error) {
+	return client.Get(context.Background(), key).Result()
+}
+
+func Del(client *redis.Client, key string) error {
+	return client.Del(context.Background(), key).Err()
+}
