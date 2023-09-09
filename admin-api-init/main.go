1470bdac94489495aadf493692fd4786954a8801
adamkoro <adam.koronics@gmail.com>
2023-09-09 08:55:03 +0200

    feat: password update

--- admin-api-init/main.go
+++ admin-api-init/main.go
@@ -20,26 +20,31 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Println("Postgres migration completed.")
-	// Admin user create
-	err = postgres.CreateUser(db, env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("Admin user created.")
 	// Check if admin user exists
 	isAdminExists, err := postgres.GetUser(db, env.GetAdminUsername())
 	if err != nil {
 		log.Println(err)
+		// Admin user create
+		err = postgres.CreateUser(db, env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Admin user created.")
 	}
 	log.Println("Admin user exists.")
-	// Update admin user if exists
+	// Update admin user if exists and password is not match
 	if isAdminExists.Username != "" {
-		err = postgres.UpdateUser(db, env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
+		err = postgres.CheckUserPassword(db, env.GetAdminUsername(), env.GetAdminPassword())
 		if err != nil {
-			log.Println(err)
+			log.Println("Admin password is not match.")
+			log.Println("Admin user password will be updated.")
+			err = postgres.UpdateUser(db, env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println("Admin user updated.")
 		}
 	}
-	log.Println("Admin user updated.")
 	// Close connection
 	defer postgres.Close(db)
 }
