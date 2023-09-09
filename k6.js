66703f5a358703e6a00abe91ab43e9a6b1ad1a23
adamkoro <adam.koronics@gmail.com>
2023-09-09 09:10:37 +0200

    chore: update k6 test

--- k6.js
+++ k6.js
@@ -1,7 +1,16 @@
 import http from "k6/http";
 
 export const options = {
-  iterations: 100,
+  scenarios: {
+    constant_request_rate: {
+      executor: 'constant-arrival-rate',
+      rate: 200,
+      timeUnit: '5s', // 1000 iterations per second, i.e. 1000 RPS
+      duration: '10s',
+      preAllocatedVUs: 10, // how large the initial pool of VUs would be
+      maxVUs: 100, // if the preAllocatedVUs are not enough, we can initialize more
+    },
+  },
 };
 
 export default function () {
