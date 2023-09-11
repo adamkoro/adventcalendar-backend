c44097509c02c46eb17b2aa7634016a1ce79aecd
adamkoro <adam.koronics@gmail.com>
2023-09-11 20:30:58 +0200

    feat: model lib

---
+++ lib/model/go.mod
@@ -0,0 +1,5 @@
+module github.com/adamkoro/adventcalendar-backend/lib/model
+
+go 1.20
+
+replace github.com/adamkoro/adventcalendar-backend/lib/model => ../lib/model
---
+++ lib/model/model.go
@@ -0,0 +1,43 @@
+package model
+
+type LoginRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type CreateUserRequest struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserRequest struct {
+	Username string `json:"username" binding:"required"`
+}
+type UserResponse struct {
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Created  string `json:"created"`
+	Modified string `json:"modified"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
+type SuccessResponse struct {
+	Status string `json:"status"`
+}
+
+type Session struct {
+	Username string
+	Token    string
+	SourceIP string
+	LoginAt  string
+}
+
+type MqMessage struct {
+	QueueName string
+	Body      []byte
+}
