3c4b8dee8d1c6a6640b3b7d11bbfccba56bf21c9
adamkoro <adam.koronics@gmail.com>
2023-09-17 17:28:16 +0200

    feat: middleware

--- email-api/publisher/api/middleware.go
+++ email-api/publisher/api/middleware.go
@@ -1,20 +1,13 @@
 package api
 
 import (
-	"context"
-	"encoding/json"
 	"log"
 	"net/http"
 
+	"github.com/adamkoro/adventcalendar-backend/lib/env"
 	custJWT "github.com/adamkoro/adventcalendar-backend/lib/jwt"
 	custModel "github.com/adamkoro/adventcalendar-backend/lib/model"
 	"github.com/gin-gonic/gin"
-	"github.com/redis/go-redis/v9"
-)
-
-var (
-	SecretKey string
-	Rd        *redis.Client
 )
 
 func AuthRequired(c *gin.Context) {
@@ -26,47 +19,9 @@ func AuthRequired(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, &errorresp)
 		return
 	}
-	claims, err := custJWT.ValidateJWT(cookie, c)
+	_, err = custJWT.ValidateJWT(cookie, env.GetSecretKey())
 	if err != nil {
 		errormessage := "Error validating JWT: " + err.Error()
-		log.Println(errormessage)
-		errorresp := custModel.ErrorResponse{Error: errormessage}
-		c.AbortWithStatusJSON(http.StatusUnauthorized, &errorresp)
-		return
-	}
-	if claims["authorized"] != true {
-		errormessage := "Unauthorized"
-		log.Println(errormessage)
-		errorresp := custModel.ErrorResponse{Error: errormessage}
-		c.AbortWithStatusJSON(http.StatusUnauthorized, &errorresp)
-		return
-	}
-	if claims["session"] == "" {
-		errormessage := "Session not found"
-		log.Println(errormessage)
-		errorresp := custModel.ErrorResponse{Error: errormessage}
-		c.AbortWithStatusJSON(http.StatusUnauthorized, &errorresp)
-		return
-	}
-	backend_session, err := Rd.Get(context.Background(), claims["session"].(string)).Bytes()
-	if err != nil {
-		errormessage := "Error getting session from redis: " + err.Error()
-		log.Println(errormessage)
-		errorresp := custModel.ErrorResponse{Error: errormessage}
-		c.AbortWithStatusJSON(http.StatusInternalServerError, &errorresp)
-		return
-	}
-	var tokenSession custModel.Session
-	err = json.Unmarshal(backend_session, &tokenSession)
-	if err != nil {
-		errormessage := "Error unmarshalling session: " + err.Error()
-		log.Println(errormessage)
-		errorresp := custModel.ErrorResponse{Error: errormessage}
-		c.AbortWithStatusJSON(http.StatusInternalServerError, &errorresp)
-		return
-	}
-	if cookie != tokenSession.Token {
-		errormessage := "Session invalid"
 		log.Println(errormessage)
 		errorresp := custModel.ErrorResponse{Error: errormessage}
 		c.AbortWithStatusJSON(http.StatusUnauthorized, &errorresp)
@@ -76,8 +31,8 @@ func AuthRequired(c *gin.Context) {
 }
 
 func CORSMiddleware() gin.HandlerFunc {
+	log.Println("CORS enabled")
 	return func(c *gin.Context) {
-		log.Println("CORS enabled")
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3030")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, Access-Control-Allow-Credentials")
