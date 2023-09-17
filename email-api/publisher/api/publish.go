c9cf6c5a15e2e8b5b8fbbc8e4146e51b409fa0a2
adamkoro <adam.koronics@gmail.com>
2023-09-17 17:28:10 +0200

    feat: rabbitmq publish

--- email-api/publisher/api/publish.go
+++ email-api/publisher/api/publish.go
@@ -1,12 +1,25 @@
 package api
 
 import (
+	"encoding/json"
+
+	"github.com/adamkoro/adventcalendar-backend/lib/model"
+	rabbitMQ "github.com/adamkoro/adventcalendar-backend/lib/rabbitmq"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-var (
-	MqQueue amqp.Queue
-)
-
-func SendMessage() {
+func SendMessage(channel *amqp.Channel, emailTo, subject, message string) error {
+	messageJson, err := json.Marshal(model.MQMessage{
+		EmailTo: emailTo,
+		Subject: subject,
+		Message: message,
+	})
+	if err != nil {
+		return err
+	}
+	err = rabbitMQ.Publish(channel, "email", messageJson)
+	if err != nil {
+		return err
+	}
+	return nil
 }
