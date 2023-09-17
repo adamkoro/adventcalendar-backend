ee1d3c3537bc3283e683a770b3162e95842de296
adamkoro <adam.koronics@gmail.com>
2023-09-17 17:27:59 +0200

    chore: one endpoint file

---
+++ email-api/publisher/api/endpoints.go
@@ -0,0 +1,31 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/adamkoro/adventcalendar-backend/lib/model"
+	"github.com/gin-gonic/gin"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var (
+	MqChannel *amqp.Channel
+)
+
+func Ping(c *gin.Context) {
+	c.String(http.StatusOK, "pong")
+}
+
+func SendMessageToRabbitMQ(c *gin.Context) {
+	var message model.MQMessage
+	if err := c.ShouldBindJSON(&message); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	err := SendMessage(MqChannel, message.EmailTo, message.Subject, message.Message)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Message sent to RabbitMQ."})
+}
