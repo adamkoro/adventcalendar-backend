851589c8688a1de3761ab7e6f4a8ce2f01c12a69
adamkoro <adam.koronics@gmail.com>
2023-09-18 15:14:06 +0200

    feat: new endpoints

--- email-api/publisher/api/endpoints.go
+++ email-api/publisher/api/endpoints.go
@@ -3,6 +3,7 @@ package api
 import (
 	"net/http"
 
+	db "github.com/adamkoro/adventcalendar-backend/lib/mariadb"
 	"github.com/adamkoro/adventcalendar-backend/lib/model"
 	"github.com/gin-gonic/gin"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -11,13 +12,33 @@ import (
 var (
 	MqChannel *amqp.Channel
 	MqQUeue   amqp.Queue
+	Db        db.Repository
 )
 
 func Ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
 
-func SendMessageToRabbitMQ(c *gin.Context) {
+func EmailSend(c *gin.Context) {
+	var rMail model.EmailRequest
+	if err := c.ShouldBindJSON(&rMail); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	mail, err := Db.GetEmailByName(rMail.Name)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	err = SendMessage(MqChannel, MqQUeue.Name, mail.To, mail.Subject, mail.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Message sent to RabbitMQ."})
+}
+
+func CustomEmailSend(c *gin.Context) {
 	var message model.MQMessage
 	if err := c.ShouldBindJSON(&message); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -25,8 +46,35 @@ func SendMessageToRabbitMQ(c *gin.Context) {
 	}
 	err := SendMessage(MqChannel, MqQUeue.Name, message.EmailTo, message.Subject, message.Message)
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"message": "Message sent to RabbitMQ."})
+}
+
+func DeleteEmail(c *gin.Context) {
+	var rMail model.EmailRequest
+	if err := c.ShouldBindJSON(&rMail); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	if rMail.Name == "default" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Can not delete default email."})
+		return
+	}
+	err := Db.DeleteEmailByName(rMail.Name)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Email deleted."})
+}
+
+func GetEmails(c *gin.Context) {
+	emails, err := Db.GetAllEmails()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, emails)
 }
