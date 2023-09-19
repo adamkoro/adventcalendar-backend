543c716f3390215769a555d4d4baa5db53ec49bc
adamkoro <adam.koronics@gmail.com>
2023-09-19 18:33:10 +0200

    fix: struct with new validation

--- lib/mongo/struct.go
+++ lib/mongo/struct.go
@@ -8,11 +8,11 @@ import (
 )
 
 type AdventCalendarDay struct {
-	ID      primitive.ObjectID `bson:"_id,omitempty"`
-	Day     int32              `bson:"day,omitempty"`
-	Year    int32              `bson:"year,omitempty"`
-	Title   string             `bson:"title,omitempty"`
-	Content string             `bson:"content,omitempty"`
+	ID      primitive.ObjectID `bson:"_id,omitempty" json:"id" `
+	Day     uint8              `bson:"day,omitempty" json:"day,omitempty" binding:"required" validate:"required,min=1,max=31"`
+	Year    uint16             `bson:"year,omitempty" json:"year,omitempty" binding:"required" validate:"required,min=1,max=9999"`
+	Title   string             `bson:"title,omitempty" json:"title,omitempty" binding:"required" validate:"required,min=1,max=255"`
+	Content string             `bson:"content,omitempty" json:"content,omitempty" binding:"required" validate:"required,min=1,max=65555"`
 }
 
 type Repository struct {
