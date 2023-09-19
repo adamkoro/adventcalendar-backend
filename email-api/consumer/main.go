2cea9be38dfec45c9f1221ff347fbdd5386b1f2b
adamkoro <adam.koronics@gmail.com>
2023-09-19 09:20:57 +0200

    feat: api and metrics endpoint

--- email-api/consumer/main.go
+++ email-api/consumer/main.go
@@ -1,24 +1,37 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
+	"net/http"
 	"net/smtp"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
 	"github.com/adamkoro/adventcalendar-backend/lib/env"
 	"github.com/adamkoro/adventcalendar-backend/lib/model"
 	rabbitMQ "github.com/adamkoro/adventcalendar-backend/lib/rabbitmq"
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 var (
-	rabbitConn *amqp.Connection
-	channel    *amqp.Channel
-	queue      amqp.Queue
-	forever    chan struct{}
+	rabbitConn  *amqp.Connection
+	channel     *amqp.Channel
+	queue       amqp.Queue
+	forever     chan struct{}
+	httpPort    int
+	metricsPort int
 )
 
 func main() {
+	httpPort = env.GetHttpPort()
+	metricsPort = env.GetMetricsPort()
 	rabbitConn, err := createRabbitMqConnection()
 	if err != nil {
 		log.Println(err)
@@ -54,7 +67,70 @@ func main() {
 			log.Println("Email sent")
 		}
 	}()
-	<-forever
+	// Api server
+	router := gin.New()
+	router.Use(gin.Recovery())
+	router.Use(gin.Logger())
+	api := router.Group("/api")
+	{
+		// Public endpoints
+		// Health check
+		api.GET("/ping", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{"message": "pong"})
+		})
+		api.GET("/health", func(c *gin.Context) {
+			if env.GetSmtpAuth() {
+			}
+			c.JSON(http.StatusOK, gin.H{"message": "ok"})
+		})
+	}
+	api_server := &http.Server{
+		Addr:         ":" + strconv.Itoa(httpPort),
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Metrics server
+	metrics := gin.New()
+	metrics.GET("/metrics", gin.WrapH(promhttp.Handler()))
+	metrics_server := &http.Server{
+		Addr:         ":" + strconv.Itoa(metricsPort),
+		Handler:      metrics,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Println("Starting server...")
+	log.Println("Listening api on port " + strconv.Itoa(httpPort))
+	log.Println("Listening metrics on port " + strconv.Itoa(metricsPort))
+	go func() {
+		// api
+		if err := api_server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+	go func() {
+		// metrics
+		if err := metrics_server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := api_server.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	select {
+	case <-ctx.Done():
+		log.Println("timeout of 5 seconds.")
+	}
+	log.Println("Server exiting")
 }
 
 func createRabbitMqConnection() (*amqp.Connection, error) {
