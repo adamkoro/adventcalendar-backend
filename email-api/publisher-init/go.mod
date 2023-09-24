94b5b4246fa003ff9dc7974cfe04bd2fde30855b
adamkoro <adam.koronics@gmail.com>
2023-09-24 18:35:15 +0200

    feat: connection wait and check, json logging

--- email-api/publisher-init/go.mod
+++ email-api/publisher-init/go.mod
@@ -10,12 +10,16 @@ require (
 	github.com/adamkoro/adventcalendar-backend/lib/env v0.0.0-00010101000000-000000000000
 	github.com/adamkoro/adventcalendar-backend/lib/mariadb v0.0.0-00010101000000-000000000000
 	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/rs/zerolog v1.30.0
 )
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
 	gorm.io/driver/mysql v1.5.1 // indirect
 	gorm.io/gorm v1.25.4 // indirect
 )
--- email-api/publisher-init/main.go
+++ email-api/publisher-init/main.go
@@ -2,51 +2,115 @@ package main
 
 import (
 	"context"
-	"log"
 	"time"
 
 	"github.com/adamkoro/adventcalendar-backend/lib/env"
 	md "github.com/adamkoro/adventcalendar-backend/lib/mariadb"
 	"github.com/common-nighthawk/go-figure"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	db          md.Repository
+	isConnected bool
+	email       md.Email
 )
 
 func main() {
 	figure.NewFigure("AdventCalendar Email Publisher Init", "big", false).Print()
-	// MariaDB connection
-	log.Println("Connecting to the MariaDB...")
-	var db *md.Repository
-	conn, err := db.Connect(env.GetDbUser(), env.GetDbPassword(), env.GetDbHost(), env.GetDbName(), env.GetDbPort())
-	if err != nil {
-		log.Fatal(err)
+	/////////////////////////
+	// Logger setup
+	/////////////////////////
+	logLevel := env.GetLogLevel()
+	switch logLevel {
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	db = md.NewRepository(conn, &ctx)
-	log.Println("Connected to the MariaDB.")
-	// MariaDB migration
-	err = db.Migrate()
+	/////////////////////////
+	// Mariadb connection check
+	/////////////////////////
+	log.Debug().Msg("establishing connection to the mariadb...")
+	mariadbConn, err := db.Connect(env.GetDbUser(), env.GetDbPassword(), env.GetDbHost(), env.GetDbName(), env.GetDbPort())
 	if err != nil {
-		log.Fatal(err)
+		log.Error().Msg(err.Error())
+	} else {
+		log.Info().Msg("connected to the mariadb")
 	}
-	// Check if default email exists
-	_, err = db.GetEmailByName("default")
-	if err != nil {
-		log.Println("Failed to get default email.")
-		log.Println("Default email will be created.")
-		var email md.Email
-		email.Name = "default"
-		email.From = "adventcalendar@localhost"
-		email.To = "adventcalendar@localhost"
-		email.Subject = "Advent Calendar"
-		email.Body = "Hello, World!"
-		err = db.CreateEmail(&email)
+	log.Debug().Msg("establishing connection to the mariadb successful")
+	ctx := context.Background()
+	log.Debug().Msg("creating mariadb repository...")
+	db := md.NewRepository(mariadbConn, &ctx)
+	log.Debug().Msg("creating mariadb repository successful")
+	isConnected = true
+	for {
+		log.Debug().Msg("pinging the mariadb...")
+		err := db.Ping()
 		if err != nil {
-			log.Fatal(err)
+			log.Info().Msg("lost connection to the mariadb, reconnecting...")
+			log.Error().Msg(err.Error())
+			isConnected = false
+		} else {
+			log.Debug().Msg("pinging the mariadb successful")
+			if !isConnected {
+				log.Info().Msg("reconnected to the mariadb")
+				isConnected = true
+			}
 		}
-		log.Println("Default email has been created.")
-		return
+		time.Sleep(5 * time.Second)
+		if isConnected {
+			/////////////////////////
+			// Database migration
+			/////////////////////////
+			log.Debug().Msg("database struct migration started...")
+			err := db.Migrate()
+			if err != nil {
+				log.Fatal().Msg(err.Error())
+			}
+			log.Info().Msg("database struct migration successful")
+			/////////////////////////
+			// Check if default email exists
+			/////////////////////////
+			log.Debug().Msg("checking if default email exists...")
+			_, err = db.GetEmailByName("default")
+			if err != nil {
+				log.Info().Msg("default email does not exist")
+				log.Info().Msg("creating default email...")
+				email.Name = "default"
+				email.From = "adventcalendar@localhost"
+				email.To = "adventcalendar@localhost"
+				email.Subject = "Advent Calendar"
+				email.Body = "Hello, World!"
+				log.Debug().Msg("creating default email in database...")
+				err = db.CreateEmail(&email)
+				if err != nil {
+					log.Fatal().Msg(err.Error())
+				}
+				log.Info().Msg("default email created")
+				return
+			} else {
+				log.Info().Msg("default email exists")
+			}
+			log.Info().Msg("all required actions completed successfully")
+			log.Debug().Msg("closing connection to the postgres...")
+			/////////////////////////
+			// Close connection
+			/////////////////////////
+			db.Close()
+			log.Debug().Msg("closing connection to the postgres successful")
+			break
+		}
 	}
-	log.Println("Default email exists.")
-	// Close MariaDB connection
-	defer db.Close()
 }
