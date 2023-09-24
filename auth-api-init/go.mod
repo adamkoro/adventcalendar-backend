64248a4032185d0c238dde8dec88d5b78b4ca173
adamkoro <adam.koronics@gmail.com>
2023-09-24 18:17:54 +0200

    feat: connection wait and check, json logging

--- auth-api-init/go.mod
+++ auth-api-init/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/adamkoro/adventcalendar-backend/lib/env v0.0.0-00010101000000-000000000000
 	github.com/adamkoro/adventcalendar-backend/lib/postgres v0.0.0-00010101000000-000000000000
 	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be
+	github.com/rs/zerolog v1.30.0
 )
 
 require (
@@ -16,7 +17,10 @@ require (
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	gorm.io/driver/postgres v1.5.2 // indirect
 	gorm.io/gorm v1.25.4 // indirect
--- auth-api-init/main.go
+++ auth-api-init/main.go
@@ -2,57 +2,126 @@ package main
 
 import (
 	"context"
-	"log"
+	"time"
 
 	"github.com/adamkoro/adventcalendar-backend/lib/env"
-	"github.com/adamkoro/adventcalendar-backend/lib/postgres"
+	pg "github.com/adamkoro/adventcalendar-backend/lib/postgres"
 	"github.com/common-nighthawk/go-figure"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+var (
+	db          pg.Repository
+	isConnected bool
 )
 
 func main() {
 	figure.NewFigure("AdventCalendar Auth Api Init", "big", false).Print()
 	// Postgres connection
-	var db *postgres.Repository
-	conn, err := db.Connect(env.GetDbHost(), env.GetDbUser(), env.GetDbPassword(), env.GetDbName(), env.GetDbPort(), env.GetDbSslMode())
-	if err != nil {
-		log.Fatal(err)
+	/////////////////////////
+	// Logger setup
+	/////////////////////////
+	logLevel := env.GetLogLevel()
+	switch logLevel {
+	case "panic":
+		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+	case "fatal":
+		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+	case "error":
+		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+	case "info":
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	case "debug":
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	case "warn":
+		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+	default:
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
+	/////////////////////////
+	// Postgres connection check
+	/////////////////////////
+	log.Debug().Msg("establishing connection to the postgres...")
+	postgresConn, err := db.Connect(env.GetDbHost(), env.GetDbUser(), env.GetDbPassword(), env.GetDbName(), env.GetDbPort(), env.GetDbSslMode())
+	if err != nil {
+		log.Error().Msg(err.Error())
+	} else {
+		log.Info().Msg("connected to the postgres")
+	}
+	log.Debug().Msg("establishing connection to the postgres successful")
 	ctx := context.Background()
-	db = postgres.NewRepository(conn, &ctx)
-	log.Println("Postgres connection established.")
-	// Migrate
-	err = db.Migrate()
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("Postgres migration completed.")
-	// Check if admin user exists
-	isAdminExists, err := db.GetUser(env.GetAdminUsername())
-	if err != nil {
-		log.Println("Admin user does not exist.")
-		log.Println("Admin user will be created.")
-		// Admin user create
-		err = db.CreateUser(env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
+	log.Debug().Msg("creating postgres repository...")
+	db := pg.NewRepository(postgresConn, &ctx)
+	log.Debug().Msg("creating postgres repository successful")
+	isConnected = true
+	for {
+		log.Debug().Msg("pinging the postgres...")
+		err := db.Ping()
 		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("Admin user created.")
-		return
-	}
-	log.Println("Admin user exists.")
-	// Update admin user if exists and password is not match
-	if isAdminExists.Username != "" {
-		err = db.CheckUserPassword(env.GetAdminUsername(), env.GetAdminPassword())
-		if err != nil {
-			log.Println("Admin password is not match.")
-			log.Println("Admin user password will be updated.")
-			err = db.UpdateUser(env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
-			if err != nil {
-				log.Fatal(err)
+			log.Info().Msg("lost connection to the postgres, reconnecting...")
+			log.Error().Msg(err.Error())
+			isConnected = false
+		} else {
+			log.Debug().Msg("pinging the postgres successful")
+			if !isConnected {
+				log.Info().Msg("reconnected to the postgres")
+				isConnected = true
 			}
-			log.Println("Admin user updated.")
+		}
+		time.Sleep(5 * time.Second)
+		if isConnected {
+			/////////////////////////
+			// Database migration
+			/////////////////////////
+			log.Debug().Msg("database struct migration started...")
+			err := db.Migrate()
+			if err != nil {
+				log.Fatal().Msg(err.Error())
+			}
+			log.Info().Msg("database struct migration successful")
+			/////////////////////////
+			// Check if admin user exists
+			/////////////////////////
+			isAdminExists, err := db.GetUser(env.GetAdminUsername())
+			if err != nil {
+				log.Info().Msg("admin user does not exists")
+				log.Info().Msg("admin user creating...")
+				// Admin user create
+				err = db.CreateUser(env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
+				if err != nil {
+					log.Fatal().Msg(err.Error())
+				}
+				log.Info().Msg("admin user created")
+				return
+			}
+			log.Info().Msg("admin user is exists")
+			/////////////////////////
+			// Update admin user if exists and password is not match
+			/////////////////////////
+			if isAdminExists.Username != "" {
+				log.Debug().Msg("checking admin user password...")
+				err = db.CheckUserPassword(env.GetAdminUsername(), env.GetAdminPassword())
+				if err != nil {
+					log.Info().Msg("admin user password is not match")
+					log.Info().Msg("admin user password updating...")
+					err = db.UpdateUser(env.GetAdminUsername(), env.GetAdminEmail(), env.GetAdminPassword())
+					if err != nil {
+						log.Fatal().Msg(err.Error())
+					}
+					log.Info().Msg("admin user password updated")
+				}
+				log.Info().Msg("admin user password is match")
+				log.Debug().Msg("checking admin user password successful")
+			}
+			log.Info().Msg("all required actions completed successfully")
+			log.Debug().Msg("closing connection to the postgres...")
+			/////////////////////////
+			// Close connection
+			/////////////////////////
+			db.Close()
+			log.Debug().Msg("closing connection to the postgres successful")
+			break
 		}
 	}
-	// Close connection
-	defer db.Close()
 }
