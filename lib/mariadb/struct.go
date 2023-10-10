287919d39f9ce3ac19c0f19d2be7f3397696e725
adamkoro <adam.koronics@gmail.com>
2023-10-10 18:21:08 +0200

    fix: validate input

--- lib/mariadb/struct.go
+++ lib/mariadb/struct.go
@@ -9,8 +9,8 @@ import (
 
 type Email struct {
 	Key        uint      `gorm:"primaryKey" json:"key"`
-	Name       string    `gorm:"type:varchar(255);unique;not null" json:"name" binding:"required" validate:"required,min=1,max=255"`
-	From       string    `gorm:"type:varchar(255);not null" json:"from" binding:"required" validate:"required,min=1,max=255"`
+	Name       string    `gorm:"type:varchar(255);unique;not null" json:"name" binding:"required" validate:"required,min=1,max=255,alpha"`
+	From       string    `gorm:"type:varchar(255);not null" json:"from" binding:"required" validate:"required,min=1,max=255,email"`
 	To         string    `gorm:"type:varchar(5000);not null" json:"to" binding:"required" validate:"required,min=1,max=5000"`
 	Subject    string    `gorm:"type:varchar(255);not null" json:"subject" binding:"required" validate:"required,min=1,max=255"`
 	Body       string    `gorm:"type:varchar(5000);not null" json:"body" binding:"required" validate:"required,min=1,max=5000"`
@@ -18,15 +18,15 @@ type Email struct {
 	ModifiedAt time.Time `gorm:"autoUpdateTime" json:"modified_at"`
 }
 type UpdateEmailRequest struct {
-	Key     uint   `json:"key"`
-	Name    string `json:"name"`
-	From    string `json:"from"`
-	To      string `json:"to"`
-	Subject string `json:"subject"`
-	Body    string `json:"body"`
+	Key     uint   `json:"key" binding:"required"`
+	Name    string `json:"name" validate:"omitempty,min=1,max=255,alpha"`
+	From    string `json:"from" validate:"omitempty,min=1,max=255,email"`
+	To      string `json:"to" validate:"omitempty,min=1,max=5000"`
+	Subject string `json:"subject" validate:"omitempty,min=1,max=255"`
+	Body    string `json:"body" validate:"omitempty,min=1,max=5000"`
 }
 type DeleteEmailRequest struct {
-	Name string `json:"name" binding:"required" validate:"required,min=1,max=255"`
+	Name string `json:"name" binding:"required" validate:"required,min=1,max=255,alpha"`
 }
 type MQMessage struct {
 	EmailTo string `json:"emailto" binding:"required" validate:"required,min=1,max=5000"`
@@ -34,7 +34,7 @@ type MQMessage struct {
 	Message string `json:"message" binding:"required" validate:"required,min=1,max=5000"`
 }
 type EmailRequest struct {
-	Name string `json:"name" binding:"required" validate:"required,min=1,max=255"`
+	Name string `json:"name" binding:"required" validate:"required,min=1,max=255,alpha"`
 }
 type Repository struct {
 	Db  *gorm.DB
--- lib/postgres/structs.go
+++ lib/postgres/structs.go
@@ -9,27 +9,27 @@ import (
 
 type User struct {
 	Key        uint      `gorm:"primaryKey"`
-	Username   string    `gorm:"type:varchar(32);unique;not null" validate:"required,min=1,max=32"`
-	Email      string    `gorm:"type:varchar(255);unique;not null" validate:"required,min=1,max=255"`
+	Username   string    `gorm:"type:varchar(32);unique;not null" validate:"required,min=1,max=32,alphanum"`
+	Email      string    `gorm:"type:varchar(255);unique;not null" validate:"required,min=1,max=255,email"`
 	Password   string    `gorm:"type:varchar(255);not null" validate:"required,min=1,max=255"`
 	CreatedAt  time.Time `gorm:"autoCreateTime"`
 	ModifiedAt time.Time `gorm:"autoUpdateTime"`
 }
 type CreateUserRequest struct {
-	Username string `json:"username" binding:"required" validate:"required,min=1,max=32"`
-	Email    string `json:"email" binding:"required" validate:"required,min=1,max=255"`
+	Username string `json:"username" binding:"required" validate:"required,min=1,max=32,alphanum"`
+	Email    string `json:"email" binding:"required" validate:"required,min=1,max=255,email"`
 	Password string `json:"password" binding:"required" validate:"required,min=1,max=255"`
 }
 type DeleteUserRequest struct {
-	Username string `json:"username" binding:"required" validate:"required,min=1,max=32"`
+	Username string `json:"username" binding:"required" validate:"required,min=1,max=32,alphanum"`
 }
 type UpdateUserRequest struct {
-	Username string `json:"username" binding:"required" validate:"required,min=1,max=32"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Username string `json:"username" binding:"required" validate:"required,min=1,max=32,alphanum"`
+	Email    string `json:"email" validate:"omitempty,min=1,max=255,email"`
+	Password string `json:"password" validate:"omitempty"`
 }
 type UserRequest struct {
-	Username string `json:"username" binding:"required" validate:"required,min=1,max=32"`
+	Username string `json:"username" binding:"required" validate:"required,min=1,max=32,alphanum"`
 }
 type UserResponse struct {
 	Id       int    `json:"id"`
@@ -39,7 +39,7 @@ type UserResponse struct {
 	Modified string `json:"modified"`
 }
 type LoginRequest struct {
-	Username string `json:"username" binding:"required" validate:"required,min=1,max=32"`
+	Username string `json:"username" binding:"required" validate:"required,min=1,max=32,alphanum"`
 	Password string `json:"password" binding:"required" validate:"required,min=1,max=255"`
 }
 type Repository struct {
